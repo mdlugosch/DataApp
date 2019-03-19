69e18d4e42aff3789f72d1eb27628684287e1998
Dlugosch <Michael.Dlugosch@web.de>
2019-03-19 16:00:50 +0100

    Kapitel 11 erste Version

---
+++ DataApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using DataApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFDatabaseContext context;
+
+        // Datencontext wird von MVC über Dependeny Injection übergeben.
+        public HomeController(EFDatabaseContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            return View(context.Products);
+        }
+    }
+}
---
+++ DataApp/Models/EFDatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataApp.Models
+{
+    /*
+     * Contextklasse versorgt EF mit Details zu den Datenmodellen
+     * die in der Datenbank abgebildet werden sollen.
+     */
+    public class EFDatabaseContext : DbContext
+    {
+        // Ein Konstruktor muss immer eingebunden werden selbst wenn er leer ist, andernfalls wird eine Fehlermeldung geworfen.
+        public EFDatabaseContext(DbContextOptions<EFDatabaseContext> opts) : base(opts) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
