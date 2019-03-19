60c82290e30b85c1250b80e42ec5a045395722e1
Dlugosch <Michael.Dlugosch@web.de>
2019-03-19 16:28:19 +0100

    cap 11 v 1.2

--- DataApp/Startup.cs
+++ DataApp/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,7 +22,8 @@ namespace DataApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            string conString = Configuration[]
+            string conString = Configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<EFDatabaseContext>(options => options.UseSqlServer(conString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
